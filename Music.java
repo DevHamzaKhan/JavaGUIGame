5c24e03c3d19db83927440ff2e47f6ce4f65849b
Zack-21-a <62900111+Zack-21-a@users.noreply.github.com>
2024-01-16 23:27:03 -0500

    Fixed Music
    Lowered volume
    Changed shot

--- Music.java
+++ Music.java
@@ -1,41 +1,32 @@
-// Use to play music
-
-// MP3s will probably not work
-
-// used this website for help with the music
-// https://www.geeksforgeeks.org/play-audio-file-using-java/
 
 import java.io.File;
 import javax.sound.sampled.*;
 
 public class Music {
-
     private Clip clip;
-
-    // play a sound and you can specifiy how many times it repeats. Setting how many
-    // times it loops to -1 will make it loop forever
-    public Music(String filePath, int loop) {
+    float vol;
+    FloatControl fc;
+    public Music(String filePath, int loopLength) {
         try {
             clip = AudioSystem.getClip();
             clip.open(AudioSystem.getAudioInputStream(new File(filePath)));
+            fc = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            vol = (float) (Math.log(0.1D) / Math.log(10.0) * 20.0);
+            fc.setValue(vol);
 
-            if (loop == -1)
+            if (loopLength == -1)
                 clip.loop(Clip.LOOP_CONTINUOUSLY);
             else
-                clip.loop(loop);
-			
+                clip.loop(loopLength);
 			stop();
         } catch (Exception e) {
-            System.out.println("Something went wrong with starting the music" + e);
+            System.out.println("Something went wrong with the music" + e);
         }
     }
-
     public void stop() {
         clip.stop();
     }
-
     public void start() {
         clip.start();
     }
-
 }
